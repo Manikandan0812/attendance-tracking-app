4ed092cc1ace42cc8007fbb5ae63940d89ba607b
Manikandan-simco <manikandan.murugan@simco.com>
2026-04-21 12:36:29 +0530

    Update logs_view.py

--- attendance/attendance/attendance_app/logs_view.py
+++ attendance/attendance/attendance_app/logs_view.py
@@ -650,7 +650,7 @@ def logs(request):
             LEFT JOIN person_shift ps 
                 ON ag.per_id = ps.per_id
 
-            WHERE ag.person_name NOT IN ('Unknown', 'Naveen')
+            WHERE ag.person_name NOT IN ('Unknown', 'Naveen' , 'Dhiwakaran')
 
             ORDER BY ag.attendance_date, ag.first_check_in DESC
         """
